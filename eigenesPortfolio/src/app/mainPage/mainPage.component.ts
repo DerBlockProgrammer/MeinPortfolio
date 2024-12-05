25c0323c3ad25f9345a95a4d8cd45aa372a4041c
DerBlockProgrammer <marcvossler@outlook.de>
2024-12-05 13:25:13 +0100

    SCSS

--- eigenesPortfolio/src/app/mainPage/mainPage.component.ts
+++ eigenesPortfolio/src/app/mainPage/mainPage.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { TitelViewMeComponent } from "./titel-view-me/titel-view-me.component";
 import { GithubLinksComponent } from "./github-links/github-links.component";
+import { AboutMeComponent } from "./about-me/about-me.component";
 
 @Component({
   selector: 'app-main',
-  imports: [TitelViewMeComponent, GithubLinksComponent],
+  imports: [TitelViewMeComponent, GithubLinksComponent, AboutMeComponent],
   templateUrl: './mainPage.component.html',
   styleUrl: './mainPage.component.scss'
 })
