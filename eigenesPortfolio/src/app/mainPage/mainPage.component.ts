be1eb2152942d2acd2908c70a44e501d72717c36
DerBlockProgrammer <marcvossler@outlook.de>
2024-12-06 11:25:29 +0100

    portfolio

--- eigenesPortfolio/src/app/mainPage/mainPage.component.ts
+++ eigenesPortfolio/src/app/mainPage/mainPage.component.ts
@@ -3,10 +3,11 @@ import { TitelViewMeComponent } from "./titel-view-me/titel-view-me.component";
 import { GithubLinksComponent } from "./github-links/github-links.component";
 import { AboutMeComponent } from "./about-me/about-me.component";
 import { SkillsComponent } from "./skills/skills.component";
+import { PortfolioComponent } from "./portfolio/portfolio.component";
 
 @Component({
   selector: 'app-main',
-  imports: [TitelViewMeComponent, GithubLinksComponent, AboutMeComponent, SkillsComponent],
+  imports: [TitelViewMeComponent, GithubLinksComponent, AboutMeComponent, SkillsComponent, PortfolioComponent],
   templateUrl: './mainPage.component.html',
   styleUrl: './mainPage.component.scss'
 })
