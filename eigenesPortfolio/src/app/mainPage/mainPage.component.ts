f8295ec2bbd2bd9afaa4671ca256688e6d69d9a8
DerBlockProgrammer <marcvossler@outlook.de>
2024-12-10 11:41:55 +0100

    Contact

--- eigenesPortfolio/src/app/mainPage/mainPage.component.ts
+++ eigenesPortfolio/src/app/mainPage/mainPage.component.ts
@@ -5,10 +5,12 @@ import { AboutMeComponent } from "./about-me/about-me.component";
 import { SkillsComponent } from "./skills/skills.component";
 import { PortfolioComponent } from "./portfolio/portfolio.component";
 import { RezessionComponent } from "./rezession/rezession.component";
+import { ContactComponent } from "./contact/contact.component";
+import { ContactLineComponent } from "./contact-line/contact-line.component";
 
 @Component({
   selector: 'app-main',
-  imports: [TitelViewMeComponent, GithubLinksComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, RezessionComponent],
+  imports: [TitelViewMeComponent, GithubLinksComponent, AboutMeComponent, SkillsComponent, PortfolioComponent, RezessionComponent, ContactComponent, ContactLineComponent],
   templateUrl: './mainPage.component.html',
   styleUrl: './mainPage.component.scss'
 })
