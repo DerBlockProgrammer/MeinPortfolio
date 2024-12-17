c3d49c3170cb0bddce35a54c5ddef70e803768a3
DerBlockProgrammer <marcvossler@outlook.de>
2024-12-17 17:47:09 +0100

    responsive!

--- eigenesPortfolio/src/app/mainPage/portfolio/portfolio.component.ts
+++ eigenesPortfolio/src/app/mainPage/portfolio/portfolio.component.ts
@@ -2,10 +2,37 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
-  imports: [],
   templateUrl: './portfolio.component.html',
-  styleUrl: './portfolio.component.scss'
+  styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent {
-
+  portfolioProjects = [
+    {
+      img: './assets/portfolio/join.png',
+      title: 'Join',
+      technologies: 'Angular | TypeScript | HTML | CSS | Firebase',
+      description:
+        'Task manager inspired by the Kanban System. Create and organize tasks using drag-and-drop functions, assign users and categories.',
+      liveLink: '#',
+      githubLink: '#'
+    },
+    {
+      img: './assets/portfolio/Sharkie.png',
+      title: 'Sharkie',
+      technologies: 'JavaScript | HTML | CSS',
+      description:
+        'A simple Jump-and-Run game based on an object-oriented approach. Help Sharkie find coins and poison bottles to fight against the killer whale.',
+      liveLink: '#',
+      githubLink: '#'
+    },
+    {
+      img: './assets/portfolio/SimpleCRM.png',
+      title: 'Simple CRM',
+      technologies: 'Angular | Firebase',
+      description:
+        'A very simple Customer Relationship Management system working with CRUD functionality.',
+      liveLink: '#',
+      githubLink: '#'
+    }
+  ];
 }
