84e8204985373a6a09ecf1430350c9fce4b93f09
DerBlockProgrammer <marcvossler@outlook.de>
2024-12-17 18:58:12 +0100

    responsive!

--- eigenesPortfolio/src/app/mainPage/portfolio/portfolio.component.ts
+++ eigenesPortfolio/src/app/mainPage/portfolio/portfolio.component.ts
@@ -1,38 +1,50 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+interface PortfolioCard {
+  image: string;
+  title: string;
+  technologies: string;
+  description: string;
+  liveLink: string;
+  githubLink: string;
+}
 
 @Component({
   selector: 'app-portfolio',
+  standalone: true, 
+  imports: [CommonModule], 
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
 })
 export class PortfolioComponent {
-  portfolioProjects = [
+  portfolioCards: PortfolioCard[] = [
     {
-      img: './assets/portfolio/join.png',
+      image: 'portfolio/join.png',
       title: 'Join',
       technologies: 'Angular | TypeScript | HTML | CSS | Firebase',
       description:
         'Task manager inspired by the Kanban System. Create and organize tasks using drag-and-drop functions, assign users and categories.',
       liveLink: '#',
-      githubLink: '#'
+      githubLink: '#',
     },
     {
-      img: './assets/portfolio/Sharkie.png',
+      image: 'portfolio/Sharkie.png',
       title: 'Sharkie',
       technologies: 'JavaScript | HTML | CSS',
       description:
         'A simple Jump-and-Run game based on an object-oriented approach. Help Sharkie find coins and poison bottles to fight against the killer whale.',
       liveLink: '#',
-      githubLink: '#'
+      githubLink: '#',
     },
     {
-      img: './assets/portfolio/SimpleCRM.png',
+      image: 'portfolio/SimpleCRM.png',
       title: 'Simple CRM',
       technologies: 'Angular | Firebase',
       description:
         'A very simple Customer Relationship Management system working with CRUD functionality.',
       liveLink: '#',
-      githubLink: '#'
-    }
+      githubLink: '#',
+    },
   ];
 }
--- eigenesPortfolio/src/app/mainPage/rezession/rezession.component.ts
+++ eigenesPortfolio/src/app/mainPage/rezession/rezession.component.ts
@@ -1,11 +1,56 @@
 import { Component } from '@angular/core';
 
+interface Testimonial {
+  quote: string;
+  author: string;
+  position: string;
+  profileImage: string;
+}
+
 @Component({
   selector: 'app-rezession',
-  imports: [],
   templateUrl: './rezession.component.html',
-  styleUrl: './rezession.component.scss'
+  styleUrls: ['./rezession.component.scss']
 })
 export class RezessionComponent {
+  testimonials: Testimonial[] = [
+    {
+      quote: "Michael really kept the team together with his great organization and clear communication.",
+      author: "V. Schuster",
+      position: "Team Partner",
+      profileImage: "assets/images/profile1.png"
+    },
+    {
+      quote: "A great leader with excellent communication skills.",
+      author: "A. Müller",
+      position: "Senior Developer",
+      profileImage: "assets/images/profile2.png"
+    },
+    {
+      quote: "An exceptional leader who always brings the team together.",
+      author: "S. Thompson",
+      position: "Project Manager",
+      profileImage: "assets/images/profile3.png"
+    }
+  ];
 
+  currentIndex: number = 0;
+
+
+  changeTestimonial(event: MouseEvent): void {
+    const clickPosition = event.offsetX / (event.currentTarget as HTMLElement).clientWidth;
+    if (clickPosition < 0.5) {
+    
+        this.currentIndex = (this.currentIndex - 1 + this.testimonials.length) % this.testimonials.length;
+    } else {
+       
+        this.currentIndex = (this.currentIndex + 1) % this.testimonials.length;
+    }
+}
+
+
+
+  get currentTestimonial(): Testimonial {
+    return this.testimonials[this.currentIndex];
+  }
 }
